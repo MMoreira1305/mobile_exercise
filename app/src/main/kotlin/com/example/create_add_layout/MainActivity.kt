f536352ba2d52e324192663fa339209a65ab9191
Matheus Moreira <134156777+MMoreira1305@users.noreply.github.com>
2024-05-05 23:02:05 -0300

    BUGFIX - Resolvendo a não aparição do nome da primeira página

--- app/src/main/kotlin/com/example/create_add_layout/MainActivity.kt
+++ app/src/main/kotlin/com/example/create_add_layout/MainActivity.kt
@@ -18,14 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-        setUpView()
-        setUpListeners()
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setUpView()
+        setUpListeners()
     }
 
 
